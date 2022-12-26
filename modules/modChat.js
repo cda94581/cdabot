625e0002c26421f309926fda13dc2e241ecd9bab
cda94581 <76138236+cda94581@users.noreply.github.com>
2022-12-25 21:18:49 -0600

    Fix thread manager

--- modules/modChat.js
+++ modules/modChat.js
@@ -2,6 +2,11 @@ import { client } from '../index.js';
 import config from '../config/config.json' assert { type: 'json' };
 const { modchat } = config;
 
+client.on('threadCreate', async thread => {
+	if (thread.parentId != modchat) return;
+	await thread.setInvitable(false);
+});
+
 client.on('threadUpdate', async (ot, thread) => {
 	if (thread.parentId != modchat) return;
 	if (thread.invitable) await thread.setInvitable(false);
